2f64a0f9be0209c0ac6c6e1ec616996f3a30edd0
Dr Theo <71725814+DrTheodor@users.noreply.github.com>
2024-02-16 21:57:52 +0300

    somewhat functional

--- src/main/java/the/grid/smp/desertum/Desertum.java
+++ src/main/java/the/grid/smp/desertum/Desertum.java
@@ -2,17 +2,20 @@ package the.grid.smp.desertum;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import the.grid.smp.desertum.config.DesertumConfig;
+import the.grid.smp.desertum.inspector.Inspector;
 import the.grid.smp.desertum.world.WorldManager;
 
 public final class Desertum extends JavaPlugin {
 
     private DesertumConfig config;
     private WorldManager worldManager;
+    private Inspector inspector;
 
     @Override
     public void onEnable() {
         this.config = new DesertumConfig(this);
         this.worldManager = new WorldManager(this);
+        this.inspector = new Inspector(this);
     }
 
     @Override
--- src/main/java/the/grid/smp/desertum/inspector/Inspector.java
+++ src/main/java/the/grid/smp/desertum/inspector/Inspector.java
@@ -1,7 +1,8 @@
 package the.grid.smp.desertum.inspector;
 
-import org.bukkit.World;
+import org.bukkit.*;
 import the.grid.smp.desertum.Desertum;
+import the.grid.smp.desertum.data.ChunkPos;
 import the.grid.smp.desertum.data.Region;
 import the.grid.smp.desertum.util.WorldUtil;
 
@@ -13,7 +14,7 @@ public class Inspector {
 
     public Inspector(Desertum desertum) {
         this.desertum = desertum;
-        this.start();
+        this.desertum.getServer().getScheduler().runTaskLater(desertum, this::start, 1L);
     }
 
     public void start() {
@@ -21,7 +22,42 @@ public class Inspector {
         Collection<Region> regions = WorldUtil.getRegions(this.desertum, world);
 
         for (Region region : regions) {
-            region.getGeneratedChunks(world);
+            for (ChunkPos pos : region.getGeneratedChunks(world)) {
+                this.inspectChunk(world, pos);
+            }
         }
+    }
+
+    private void inspectChunk(World world, ChunkPos pos) {
+        this.inspectChunk(world.getChunkAt(pos.x(), pos.z()));
+    }
+
+    private void inspectChunk(Chunk chunk) {
+        this.inspectSnapshot(chunk.getChunkSnapshot(false, false, false));
+    }
+
+    private void inspectSnapshot(ChunkSnapshot snapshot) {
+        int startX = snapshot.getX() * 16;
+        int startZ = snapshot.getZ() * 16;
+
+        int endX = startX+15;
+        int endZ = startZ+15;
+
+        for (int x = startX; x < endX; x++) {
+            for (int y = 0; y < 320; y++) {
+                for (int z = 0; z < endZ; z++) {
+                    if (Inspector.playerMade(snapshot.getBlockType(x, y, z))) {
+                        this.desertum.getLogger().severe("Chunk at " + snapshot.getX() + ":" + snapshot.getZ() + " has player stuff! Leaving untouched...");
+                        break;
+                    }
+                }
+            }
+        }
+
+        this.desertum.getLogger().severe("Chunk at " + snapshot.getX() + ":" + snapshot.getZ() + " doesnt have player stuff! Can be purged!");
+    }
+
+    private static boolean playerMade(Material material) {
+        return Tag.BEDS.isTagged(material) || material == Material.CHEST || material == Material.ENDER_CHEST;
     }
 }
